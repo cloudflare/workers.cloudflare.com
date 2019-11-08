560c0683cf5f5d8e369db4e03b792a530190cf1f
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2019-11-08 17:29:25 -0500

    footer/header/nav css imports -> component js

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Nav from './nav'
 
+// TODO - move these to workers-brand-assets and import into workers.cloudflare.com and built-with-workers
+import '../vendor/workers.cloudflare.com/css/components/header.css'
+
 const Header = () => (
   <header class="Header">
     <Nav />
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React from 'react'
+
+// TODO - move these to workers-brand-assets and import into workers.cloudflare.com and built-with-workers
+import '../vendor/workers.cloudflare.com/css/components/nav.css'
 
 const Nav = () => (
   <nav class="Nav">
