83b8e5ec584b9c6663f4d59f76d8da1ee3eda457
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2019-11-14 13:32:55 -0500

    404 page mvp

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,21 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "../vendor/workers-brand-assets/css/components/error-page.css"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <div class="ErrorPage">
+      <div class="ErrorPage--content">
+        <h1 class="ErrorPage--title">Not found</h1>
+        <p class="ErrorPage--description">Unfortunately, the page you requested cannot be found.</p>
+        <div class="ErrorPage--action">
+          <a class="Button Button-is-primary" href="/">Home</a>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
