b9f54b3eb3dda4a8464f130484d09e8f25b2958a
Kristian Freeman <kristian@kristianfreeman.com>
2020-01-23 11:04:05 -0600

    Add bookmark indicator on index page

--- src/components/bookmark.js
+++ src/components/bookmark.js
@@ -28,14 +28,68 @@ const unbookmark = async ({ key, setBookmarked, setState }) => {
   return false
 }
 
-const hydrate = async (_state, setState) => {
+const hydrate = async ({ slug, state, setRequestedHydration, setState }) => {
   return new Promise(async resolve => {
-    const url = new URL(window.location)
-    const resp = await fetch(url.pathname + "/_hydrate")
+    setRequestedHydration(true)
+    const resp = await fetch(`/built-with/projects/${slug}/_hydrate`)
     const { bookmarks } = await resp.json()
     setState(bookmarks)
     resolve()
   })
+}
+
+const BookmarkIndicator = ({ bookmarked }) => {
+  const uid = () =>
+    `id${
+      Math.random()
+        .toString()
+        .split(".")[1]
+    }`
+  const gradientID = uid()
+  const shadowID = uid()
+  const pathID = uid()
+
+  return bookmarked ? (
+    <div className="Project--bookmark">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 34">
+        <defs>
+          <linearGradient id={gradientID} x1="76%" x2="12%" y1="0%" y2="107%">
+            <stop offset="0%" stopColor="#f8ac4a" />
+            <stop offset="100%" stopColor="#f48424" />
+          </linearGradient>
+          <filter
+            id={shadowID}
+            width="140%"
+            height="130%"
+            x="-20%"
+            y="-10%"
+            filterUnits="objectBoundingBox"
+          >
+            <feOffset dy="2" in="SourceAlpha" result="shadowOffsetOuter1" />
+            <feGaussianBlur
+              in="shadowOffsetOuter1"
+              result="shadowBlurOuter1"
+              stdDeviation="1.5"
+            />
+            <feColorMatrix
+              in="shadowBlurOuter1"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.184188179 0"
+            />
+          </filter>
+          <path
+            id={pathID}
+            d="M2 0h22a2 2 0 012 2v29.586a1 1 0 01-1.707.707L13.707 21.707a1 1 0 00-1.414 0L1.707 32.293A1 1 0 010 31.586V2a2 2 0 012-2z"
+          />
+        </defs>
+        <use
+          fill="#000"
+          filter={"url(#" + shadowID + ")"}
+          href={"#" + pathID}
+        />
+        <use fill={"url(#" + gradientID + ")"} href={"#" + pathID} />
+      </svg>
+    </div>
+  ) : null
 }
 
 const BookmarkButton = ({ bookmarked = false, onClick }) => (
@@ -50,14 +104,26 @@ const BookmarkButton = ({ bookmarked = false, onClick }) => (
   </button>
 )
 
-const Bookmark = ({ bookmarked, project, setBookmarked, setLoaded }) => {
+const Bookmark = ({
+  bookmarked,
+  project,
+  readOnly = false,
+  setBookmarked,
+  setLoaded,
+}) => {
   const key = `${project.slug}_bookmarked`
+  const [requestedHydration, setRequestedHydration] = useState(false)
   const [state, setState] = React.useContext(EdgeStateContext)
 
   useEffect(() => {
     async function parse() {
-      if (!state || !Object.keys(state).includes(key)) {
-        return hydrate(state, setState)
+      if (!requestedHydration) {
+        return hydrate({
+          slug: project.slug,
+          state,
+          setState,
+          setRequestedHydration,
+        })
       }
 
       const kvBookmarked = state[key]
@@ -70,7 +136,9 @@ const Bookmark = ({ bookmarked, project, setBookmarked, setLoaded }) => {
     setLoaded(true)
   }, [state])
 
-  return (
+  return readOnly ? (
+    <BookmarkIndicator bookmarked={bookmarked} />
+  ) : (
     <div
       className="ProjectPage--header-action-bookmark"
       data-is-bookmarked={bookmarked}
--- src/components/project.js
+++ src/components/project.js
@@ -2,34 +2,38 @@ import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 
+import Bookmark from "./bookmark"
 import "./project.css"
 
-const Project = ({
-  project: {
+const Project = ({ project }) => {
+  const {
     thumbnail: {
       asset: { fluid },
     },
     name,
     slug,
     shortDescription,
-    features,
-  },
-}) => (
-  <Link
-    className="Project---link Project---link-fills-height"
-    to={`/built-with/projects/${slug}`}
-  >
-    <div className="Project Project-fills-height">
-      <div className="Project--image">
-        <Img fluid={fluid} />
-      </div>
+  } = project
 
-      <div className="Project--content">
-        <h2 className="Project--title">{name}</h2>
-        <p className="Project--description">{shortDescription}</p>
+  return (
+    <Link
+      className="Project---link Project---link-fills-height"
+      to={`/built-with/projects/${slug}`}
+    >
+      <div className="Project Project-fills-height">
+        <div className="Project--image">
+          <Img fluid={fluid} />
+        </div>
+
+        <div className="Project--content">
+          <h2 className="Project--title">{name}</h2>
+          <p className="Project--description">{shortDescription}</p>
+        </div>
+
+        <Bookmark project={project} readOnly />
       </div>
-    </div>
-  </Link>
-)
+    </Link>
+  )
+}
 
 export default Project
