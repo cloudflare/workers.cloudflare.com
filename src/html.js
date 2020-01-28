971e49a4952977eb50886c910260e3c968680754
Ashley Williams <ashley666ashley@gmail.com>
2020-01-28 16:50:44 -0600

    feat(html): add twitter and open graph meta tags

--- src/html.js
+++ src/html.js
@@ -24,7 +24,26 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes} theme="light">
       <head>
-        <meta charSet="utf-8" />
+        <!-- COMMON TAGS -->
+        <meta charset="utf-8">
+        <title>Cloudflare - Built with Workers</title>
+        <!-- Search Engine -->
+        <meta name="image" content="https://repository-images.githubusercontent.com/215130914/0a128400-41f5-11ea-8dc8-b1c09a48fa06">
+        <!-- Schema.org for Google -->
+        <meta itemprop="name" content="Cloudflare - Built with Workers">
+        <meta itemprop="image" content="https://repository-images.githubusercontent.com/215130914/0a128400-41f5-11ea-8dc8-b1c09a48fa06">
+        <!-- Twitter -->
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="Cloudflare - Built with Workers">
+        <meta name="twitter:site" content="@cloudflaredev">
+        <meta name="twitter:creator" content="@cloudflaredev">
+        <meta name="twitter:image:src" content="https://repository-images.githubusercontent.com/215130914/0a128400-41f5-11ea-8dc8-b1c09a48fa06">
+        <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+        <meta name="og:title" content="Cloudflare - Built with Workers">
+        <meta name="og:image" content="https://repository-images.githubusercontent.com/215130914/0a128400-41f5-11ea-8dc8-b1c09a48fa06">
+        <meta name="og:url" content="https://workers.cloudflare.com/built-with/">
+        <meta name="og:site_name" content="Cloudflare- Built with Workers">
+        <meta name="og:type" content="website">
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
