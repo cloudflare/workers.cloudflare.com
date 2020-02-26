2df77cc4e9c899fc2b74d09cb6616851c03aaa84
Kristian Freeman <kristian@kristianfreeman.com>
2020-02-26 15:54:43 -0600

    Fix trailing slashes issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-remove-trailing-slashes`,
     {
       resolve: "gatsby-source-sanity",
       options: {
