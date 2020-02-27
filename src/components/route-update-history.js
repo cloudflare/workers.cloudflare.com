4f23c3444b3ef2c2f8f14555f2f9cea59e9b9854
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-02-26 19:09:35 -0500

    don’t expose window

--- src/components/route-update-history.js
+++ src/components/route-update-history.js
@@ -1,17 +1,17 @@
-window.__routeUpdateHistory = (window['__routeUpdateHistory'] || [])
-
 const onRouteUpdate = ({ location, prevLocation }) => {
+  if (!window.__routeUpdateHistory) window.__routeUpdateHistory = []
+
   window.__routeUpdateHistory.push({ location, prevLocation })
 }
 
-const getRouteUpdateHistory = () => window.__routeUpdateHistory
+const getRouteUpdateHistory = () => window.__routeUpdateHistory || []
 
 const getLastRouteUpdate = () => {
-  if (window.__routeUpdateHistory.length > 0)
+  if (window.__routeUpdateHistory && window.__routeUpdateHistory.length > 0)
     return window.__routeUpdateHistory[window.__routeUpdateHistory.length - 1]
 }
 
-const isInitialRoute = () => window.__routeUpdateHistory.length === 0
+const isInitialRoute = () => !window.__routeUpdateHistory || window.__routeUpdateHistory.length === 0
 
 export {
   onRouteUpdate,
