7de236dcdd4c3b88304f39ded65929e6b1af7d40
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-02-26 19:30:11 -0500

    fix window reference error

--- src/components/route-update-history.js
+++ src/components/route-update-history.js
@@ -1,17 +1,29 @@
-const onRouteUpdate = ({ location, prevLocation }) => {
-  if (!window.__routeUpdateHistory) window.__routeUpdateHistory = []
+const isWindowAvailable = !!(typeof window !== "undefined")
 
+if (isWindowAvailable) {
+  window.__routeUpdateHistory = window.__routeUpdateHistory || []
+}
+
+const onRouteUpdate = ({ location, prevLocation }) => {
+  if (!isWindowAvailable) return
   window.__routeUpdateHistory.push({ location, prevLocation })
 }
 
-const getRouteUpdateHistory = () => window.__routeUpdateHistory || []
+const getRouteUpdateHistory = () => {
+  if (!isWindowAvailable) return
+  return window.__routeUpdateHistory
+}
 
 const getLastRouteUpdate = () => {
-  if (window.__routeUpdateHistory && window.__routeUpdateHistory.length > 0)
+  if (!isWindowAvailable) return
+  if (window.__routeUpdateHistory.length > 0)
     return window.__routeUpdateHistory[window.__routeUpdateHistory.length - 1]
 }
 
-const isInitialRoute = () => !window.__routeUpdateHistory || window.__routeUpdateHistory.length === 0
+const isInitialRoute = () => {
+  if (!isWindowAvailable) return
+  return window.__routeUpdateHistory.length === 0
+}
 
 export {
   onRouteUpdate,
