0ac2f22f54f792f9c2db92a1549d7520eb974c2f
Kate Tungusova <ktungusova@cloudflare.com>
2021-06-14 16:55:39 +0100

    fixing CLI link

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,7 +135,7 @@ const IndexPage = () => {
                 <img alt="Templates illustration" src="./resources/illustrations/templates-dark-theme.svg" />
               </div>
               <h3 className="BenefitsSection--benefit-title">Write in JS, Rust, C, and C++</h3>
-              <div className="BenefitsSection--benefit-description">Choose from a template in your language to kickstart building an app, creating a function, or writing an API. We have <a className="Link" href="https://workers.cloudflare.com/docs/templates/">templates</a>, <a className="Link" href="https://workers.cloudflare.com/docs/tutorials/">tutorials</a>, and a <a className="Link" href="https://workers.cloudflare.com/docs/quickstart/cli-setup/">CLI</a> to get you up and running in no time.</div>
+              <div className="BenefitsSection--benefit-description">Choose from a template in your language to kickstart building an app, creating a function, or writing an API. We have <a className="Link" href="https://workers.cloudflare.com/docs/templates/">templates</a>, <a className="Link" href="https://workers.cloudflare.com/docs/tutorials/">tutorials</a>, and a <a className="Link" href="https://developers.cloudflare.com/workers/get-started/guide#2-install-the-workers-cli">CLI</a> to get you up and running in no time.</div>
             </div>
             <div className="BenefitsSection--benefit">
               <div className="BenefitsSection--benefit-illustration" light-theme-only="true">
