c1e5dd80a22fffd5150f24fedc7ed59fd150d298
Rita Kozlov <2414910+rita3ko@users.noreply.github.com>
2022-07-13 12:38:30 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -276,7 +276,11 @@ const IndexPage = () => {
               <div className="PlansSection--plan-details">
                 <ul className="UnorderedListWorkersThemed">
                   <li>Everything in Free, plus:</li>
-                  <li>Up to <strong>30s wall time</strong> per request (Need more? <a href="workers-product@cloudflare.com">contact us</a>!</li>
+                  <li>Up to <strong>30s wall time</strong> per request Need more? <a className="Link"
+                        style={{
+                          "--underline-size": "-1px",
+                          "--underline-color": "rgba(var(--color-rgb), .5)"
+                        }} href="workers-product@cloudflare.com">contact us</a>!</li>
                   <li><strong>Always lowest latency</strong></li>
                   <li>
                     Additional
