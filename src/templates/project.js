bacb3f781c5feeb6383ab83ed483e6118cc7b078
Kristian Freeman <kristian@kristianfreeman.com>
2023-07-20 08:33:27 -0500

    show more projects on project page

--- src/templates/project.js
+++ src/templates/project.js
@@ -212,7 +212,7 @@ const Project = ({
               {collectionForProject &&
                 shuffle(collectionForProject._rawProjects)
                   .filter(({ id }) => id !== project.id)
-                  .slice(0, PROJECTS_PER_COLLECTION)
+                  .slice(0, 20)
                   .map(project => (
                     <div className="Collection--project" key={project.id}>
                       <RelatedProject project={project} />
