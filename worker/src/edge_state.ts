1a1b5b4f55c245272df62ea86d6040a0ccaacfb3
Samuel Macleod <smacleod@cloudflare.com>
2023-09-15 12:18:55 +0100

    Convert worker to TS, add CI deploy script, and add tests

--- worker/src/edge_state.ts
+++ worker/src/edge_state.ts
@@ -1,19 +1,23 @@
 class EdgeStateEmbed {
-  constructor(state) {
-    this._state = state
-  }
+  constructor(private state: unknown) {}
 
-  element(element) {
+  element(element: Element) {
     const edgeStateElement = `
       <script id='edge_state' type='application/json'>
-        ${JSON.stringify(this._state)}
+        ${JSON.stringify(this.state)}
       </script>
     `
     element.prepend(edgeStateElement, { html: true })
   }
 }
 
-export const hydrateEdgeState = ({ state, response }) => {
+export const hydrateEdgeState = ({
+  state,
+  response,
+}: {
+  state: unknown
+  response: Response
+}) => {
   const rewriter = new HTMLRewriter().on("body", new EdgeStateEmbed(state))
   return rewriter.transform(response)
 }
