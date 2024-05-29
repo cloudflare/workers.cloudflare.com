755d92d5ac90d9f5883ce4c4b6e95088977c84a9
Kristian Freeman <kristian@kristianfreeman.com>
2024-05-29 14:18:57 -0500

    Update sanity

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,18 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  graphql: [
+    {
+      id: 'production',
+      workspace: 'production'
+    },
+    {
+      id: 'development',
+      workspace: 'development'
+    }
+  ]
+  // api: {
+  //   projectId: '0s2zavz0',
+  //   dataset: 'production'
+  // }
+})
---
+++ sanity/sanity.config.js
@@ -0,0 +1,46 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+import {markdownSchema} from "sanity-plugin-markdown";
+
+export default defineConfig([
+  {
+    name: 'production',
+    title: 'Built with Workers (Production)',
+    basePath: '/production',
+  
+    projectId: '0s2zavz0',
+    dataset: 'production',
+
+    scheduledPublishing: { enabled: false },
+  
+    plugins: [
+      structureTool(), 
+      markdownSchema(),
+    ],
+  
+    schema: {
+      types: schemaTypes,
+    },
+  },
+  {
+    name: 'development',
+    title: 'Built with Workers (Development)',
+    basePath: '/development',
+  
+    projectId: '0s2zavz0',
+    dataset: 'development',
+
+    scheduledPublishing: { enabled: false },
+  
+    plugins: [
+      structureTool(), 
+      markdownSchema(),
+    ],
+  
+    schema: {
+      types: schemaTypes,
+    },
+  }
+])
