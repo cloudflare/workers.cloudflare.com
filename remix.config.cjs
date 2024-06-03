dc6094a007271f3a83307ec7f1ba474e0b40db86
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-03 09:21:45 -0500

    Build sutff

--- remix.config.cjs
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('@remix-run/dev/config').AppConfig}
- */
-module.exports = {
-  serverBuildTarget: 'cloudflare-pages',
-  server: './server.js',
-  devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: ['**/.*'],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import {
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { getLoadContext } from './load-context';
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+	plugins: [
+		remixCloudflareDevProxy({ getLoadContext }),
+		remix(),
+		tsconfigPaths(),
+	],
 });
