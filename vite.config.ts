f5adb13cedae4d15ad695433cf704eaa27792f10
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-03 13:14:52 -0500

    Use config from remix starter

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,31 @@
+import { defineConfig } from "vite";
 import {
-	vitePlugin as remix,
-	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { getLoadContext } from './load-context';
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin,
+} from "@remix-run/dev";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		remixCloudflareDevProxy({ getLoadContext }),
-		remix(),
-		tsconfigPaths(),
-	],
+  plugins: [
+    cloudflareDevProxyVitePlugin(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    resolve: {
+      conditions: ["workerd", "worker", "browser"],
+    },
+  },
   resolve: {
-    alias: {
-      'stream': 'node:stream'
-    }
-  }
+    mainFields: ["browser", "module", "main"],
+  },
+  build: {
+    minify: true,
+  },
 });
