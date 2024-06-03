06dbe95670c58a611df431f7dc57d4f0cab334cc
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-03 09:18:56 -0500

    More build stuff

---
+++ remix.config.cjs
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+module.exports = {
+  serverBuildTarget: 'cloudflare-pages',
+  server: './server.js',
+  devServerBroadcastDelay: 1000,
+  ignoredRouteFiles: ['**/.*'],
+};
