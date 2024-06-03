fb732ac4479fccf590a59bb4969cee6df37958a7
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-03 13:36:28 -0500

    functioning builds

--- app/lib/sanity/index.ts
+++ app/lib/sanity/index.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: '0s2zavz0',
   dataset: 'production',
-  token: process.env.SANITY_AUTH_TOKEN,
+  token: "skmT17U6Qj930vYyXBwk2SCQC9sxHOnuTamok96EthWPZ5JuXyplSBDX082fgIeaX2KVeIfLgK9pcGc8E6B5eCyi1erjq0IguB7kSyifPzxNtf0BsyEFqwBEuL5kYloYwQIZUX15dkCF7eAkxBU5PCwtwFKZbVi7n1Ja4dLPStZLm4e733gX",
   useCdn: true,
   apiVersion: '2024-05-31'
 })
