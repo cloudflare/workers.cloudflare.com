bf2c790d7f4430d7f18127002a7a15b3bb72f434
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-03 08:58:41 -0500

    Move to parent folder

--- remix/app/isDOMavailable.js
+++
@@ -1,8 +0,0 @@
-// h/t https://react.30secondsofcode.org/snippet/useSSR
-const isDOMavailable = !!(
-  typeof window !== "undefined" &&
-  window.document &&
-  window.document.createElement
-)
-
-export default isDOMavailable
