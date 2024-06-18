9c51e8d4ac87795e5656b9460c70c33b77ff120f
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-18 13:58:55 -0500

    Simplify worker and remove lots of unused code
    This PR simplifies the worker set up for the Workers website in the
    following ways:
    
    - Remove all bookmark/edge state functionality
    - Remove unused redirects
    - Scope the route for the worker to /playground* so that the remaining
      playground functionality still works

--- worker/tests/index.test.ts
+++ worker/tests/index.test.ts
@@ -1,9 +1,7 @@
-import { randomBytes } from "crypto"
 import { resolve } from "path"
 import { describe, expect, it, beforeAll, afterAll, assert } from "vitest"
 import { unstable_dev } from "wrangler"
 import type { UnstableDevWorker } from "wrangler"
-import redirects from "../src/redirects"
 
 describe("worker", () => {
   let worker: UnstableDevWorker
@@ -19,54 +17,7 @@ describe("worker", () => {
   afterAll(async () => {
     await worker.stop()
   })
-  describe("redirects", () => {
-    describe("docs", () => {
-      it("suffix", async () => {
-        const resp = await worker.fetch(
-          `https://workers.cloudflare.com/docs/reference/runtime/limits/`,
-          {
-            redirect: "manual",
-          }
-        )
 
-        const location = resp.headers.get("Location")
-
-        expect(location).toMatchInlineSnapshot(
-          '"https://developers.cloudflare.com/workers//reference/runtime/limits/"'
-        )
-      })
-      it("prefix", async () => {
-        const resp = await worker.fetch(
-          `https://workers.cloudflare.com/nodedocs`,
-          {
-            redirect: "manual",
-          }
-        )
-
-        const location = resp.headers.get("Location")
-
-        expect(location).toMatchInlineSnapshot(
-          '"https://developers.cloudflare.com/workers/node"'
-        )
-      })
-    })
-    describe("static definitions", () => {
-      for (const redirect of redirects) {
-        it(redirect.path, async () => {
-          const resp = await worker.fetch(
-            `https://workers.cloudflare.com${redirect.path}`,
-            {
-              redirect: "manual",
-            }
-          )
-
-          const location = resp.headers.get("Location")
-
-          expect(location).toContain(redirect.redirect)
-        })
-      }
-    })
-  })
   describe("playground proxy", () => {
     it("basic", async () => {
       const resp = await worker.fetch(
@@ -77,6 +28,7 @@ describe("worker", () => {
         `<title>Cloudflare Workers Playground</title>`
       )
     })
+
     it("forwards method", async () => {
       const resp = await worker.fetch(
         `https://workers.cloudflare.com/playground/api/worker`,
@@ -93,6 +45,7 @@ describe("worker", () => {
         }
       `)
     })
+
     it("forwards cookie", async () => {
       const resp = await worker.fetch(
         `https://workers.cloudflare.com/playground/api/worker`,
@@ -114,6 +67,7 @@ describe("worker", () => {
         }
       `)
     })
+
     it("rewrites cookie domain", async () => {
       const resp = await worker.fetch(
         `https://some-random-domain.com/playground/`
@@ -122,86 +76,6 @@ describe("worker", () => {
       expect(resp.headers.get("set-cookie")).toContain(
         "Domain=some-random-domain.com"
       )
-    })
-  })
-
-  describe("bookmark", () => {
-    it("create new", async () => {
-      const project = randomBytes(20).toString("hex")
-      const resp = await worker.fetch(
-        `https://workers.cloudflare.com/built-with/projects/${project}//bookmark`,
-        {
-          method: "POST",
-        }
-      )
-      expect(resp.status).toMatchInlineSnapshot("201")
-      expect(await resp.text()).toMatchInlineSnapshot('"OK"')
-    })
-
-    it("confirm existing", async () => {
-      const project = randomBytes(20).toString("hex")
-      await worker.fetch(
-        `https://workers.cloudflare.com/built-with/projects/${project}//bookmark`,
-        {
-          method: "POST",
-        }
-      )
-      const resp = await worker.fetch(
-        `https://workers.cloudflare.com/built-with/projects/${project}//bookmark`,
-        {
-          method: "POST",
-        }
-      )
-      expect(resp.status).toMatchInlineSnapshot("304")
-      expect(await resp.text()).toMatchInlineSnapshot('""')
-    })
-
-    it("remove", async () => {
-      const project = randomBytes(20).toString("hex")
-      const resp = await worker.fetch(
-        `https://workers.cloudflare.com/built-with/projects/${project}//bookmark`,
-        {
-          method: "POST",
-        }
-      )
-      expect(resp.status).toMatchInlineSnapshot("201")
-      expect(await resp.text()).toMatchInlineSnapshot('"OK"')
-      const removeResp = await worker.fetch(
-        `https://workers.cloudflare.com/built-with/projects/${project}//unbookmark`,
-        {
-          method: "POST",
-        }
-      )
-      expect(removeResp.status).toMatchInlineSnapshot("204")
-      expect(await removeResp.text()).toMatchInlineSnapshot('""')
-    })
-  })
-
-  describe("edge state hydration", () => {
-    it("injected", async () => {
-      const resp = await worker.fetch(`https://example.com`)
-      const text = await resp.text()
-      const bookmarks =
-        /<script id='edge_state' type='application\/json'>((.|\n)*?)<\/script>/.exec(
-          text
-        )
-
-      assert(bookmarks)
-      expect(JSON.parse(bookmarks?.[1])).toHaveProperty("bookmarks")
-    })
-    it("state=null when path matches a mime type", async () => {
-      const resp = await worker.fetch(`https://example.com/page.html`)
-      const text = await resp.text()
-      const bookmarks =
-        /<script id='edge_state' type='application\/json'>((.|\n)*?)<\/script>/.exec(
-          text
-        )
-
-      expect(bookmarks?.[1]).toMatchInlineSnapshot(`
-        "
-                null
-              "
-      `)
     })
   })
 })
