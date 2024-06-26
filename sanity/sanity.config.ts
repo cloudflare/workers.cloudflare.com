356a07041084ddb661efb00e7856d0a4b16e3357
Kristian Freeman <kristian@kristianfreeman.com>
2024-06-26 10:02:41 -0500

    New stuff

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {markdownSchema} from "sanity-plugin-markdown";
 
